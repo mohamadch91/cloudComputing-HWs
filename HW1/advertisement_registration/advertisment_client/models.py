e3b394b47fd6939ded9fdc901e71deb0934cc5d8
mohamadch91 <mohamadchoupan80@gmail.com>
2022-11-08 17:55:15 +0330

    feat: create advertisment model

--- HW1/advertisement_registration/advertisment_client/models.py
+++ HW1/advertisement_registration/advertisment_client/models.py
@@ -4,7 +4,7 @@ class Advertisement(models.Model):
     id=models.AutoField(primary_key=True)
     description = models.CharField(max_length=200)
     email = models.EmailField()
-    caregory=models.CharField(max_lentgh=50)
+    category=models.CharField(max_length=50)
     state=models.CharField(max_length=50)
     image = models.ImageField(upload_to='images/')
 
