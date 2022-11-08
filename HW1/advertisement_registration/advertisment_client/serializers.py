385ce9bb44c02d734aa7d02f5e55be3d11aec406
mohamadch91 <mohamadchoupan80@gmail.com>
2022-11-08 18:13:38 +0330

    feat: add serializers

---
+++ HW1/advertisement_registration/advertisment_client/serializers.py
@@ -0,0 +1,9 @@
+
+from dataclasses import fields
+from rest_framework import serializers
+from .models import *
+
+class AdvertisementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Advertisement
+        fields = '__all__'
