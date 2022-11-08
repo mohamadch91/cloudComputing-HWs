92485a99f574db8889693150d7fefaa48d1f5738
mohamadch91 <mohamadchoupan80@gmail.com>
2022-11-08 18:32:48 +0330

    feat: add API url to project urls

--- HW1/advertisement_registration/advertisement_registration/urls.py
+++ HW1/advertisement_registration/advertisement_registration/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include, re_path
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('advertisment/', include('advertisment_client.urls')),
+
+    
 ]
