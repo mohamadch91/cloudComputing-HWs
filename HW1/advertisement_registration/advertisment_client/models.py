db0b732bc46242c8c2d9c79ad7cf216b1b39391e
mohamadch91 <mohamadchoupan80@gmail.com>
2022-11-08 18:00:34 +0330

    fix: add choices to advertisment model
    add three choice to state field

--- HW1/advertisement_registration/advertisment_client/models.py
+++ HW1/advertisement_registration/advertisment_client/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 
 class Advertisement(models.Model):
+    #create choice field for advertisement type
+    class choices(models.TextChoices):
+            accepted = "accepted"  
+            rejected = "rejected"
+            pending = "pending"
+            
     id=models.AutoField(primary_key=True)
     description = models.CharField(max_length=200)
     email = models.EmailField()
     category=models.CharField(max_length=50)
-    state=models.CharField(max_length=50)
+    state=models.CharField(max_length=50,choices=choices.choices,default=choices.pending)
     image = models.ImageField(upload_to='images/')
 
     def __str__(self):
