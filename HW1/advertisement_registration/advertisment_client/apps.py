3bfceb8bacd63527b776d0fc533d51955a8aff1d
mohamadch91 <mohamadchoupan80@gmail.com>
2022-11-20 14:53:16 +0330

    feat: add comment to task.py

--- HW1/advertisement_registration/advertisment_client/apps.py
+++ HW1/advertisement_registration/advertisment_client/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-
+# Define the app name
 class AdvertismenClientConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'advertisment_client'
